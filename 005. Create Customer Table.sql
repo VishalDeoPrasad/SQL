0f15ef6160f003000022f9221fbb6e5c2cac72d0
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2022-12-29 02:06:50 +0530

    Create Customer table

---
+++ 005. Create Customer Table.sql
@@ -0,0 +1,34 @@
+CREATE TABLE customer(
+   customer_id         INTEGER  NOT NULL PRIMARY KEY 
+  ,customer_first_name VARCHAR(7) NOT NULL
+  ,customer_last_name  VARCHAR(10) NOT NULL
+  ,customer_zip        INTEGER  NOT NULL
+);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (1,'Jane','Connor',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (2,'Manuel','Diaz',22821);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (3,'Bob','Wilson',22821);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (4,'Deanna','Washington',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (5,'Abigail','Harris',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (6,'Betty','Bullard',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (7,'Jessica','Armenta',22821);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (8,'Norma','Valenzuela',22821);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (9,'Janet','Forbes',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (10,'Russell','Edwards',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (11,'Richard','Paulson',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (12,'Jack','Wise',22821);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (13,'Jeremy','Gruber',22821);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (14,'William','Lopes',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (15,'Darrell','Messina',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (16,'Ada','Nieves',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (17,'Carlos','Diaz',22802);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (18,'Jeri','Mitchell',22802);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (19,'Iva','Kienzler',22802);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (20,'Valerie','Loftis',22802);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (21,'Duane','Sipp',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (22,'George','Rai',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (23,'Alvin','Laurie',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (24,'Dawn','Nale',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (25,'Bonnie','Hassan',22801);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (26,'Tracie','Goehring',22821);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (27,'John','Michael',22821);
+INSERT INTO customer(customer_id,customer_first_name,customer_last_name,customer_zip) VALUES (28,'Ken','Alan',22821);
---
+++ 006. Display Customer table.sql
@@ -0,0 +1,1 @@
+select * from farmers_market.dbo.customer
