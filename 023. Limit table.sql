ef584a560ebae2e124b011dee228bb38a6c07d8b
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2022-12-30 23:55:44 +0530

    question 23

---
+++ 023. Limit table.sql
@@ -0,0 +1,9 @@
+/*
+List down 10 rows of farmer’s market vendor booth assignments, displaying the
+market date, vendor ID, and booth number from the vendor_booth_assignments
+table.
+*/
+
+select * 
+from farmers_market.dbo.vendor_booth_assignments
+limit 10
