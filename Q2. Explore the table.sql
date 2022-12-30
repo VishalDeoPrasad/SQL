64eaf0130818912c25d4e88db26b74d1484b8f94
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2022-12-30 21:48:01 +0530

    Question 2

---
+++ Q2. Explore the table.sql
@@ -0,0 +1,9 @@
+/*
+ Question: Explore vendor_booth_assignments.
+List down 10 rows of farmer’s market vendor booth assignments, displaying the
+market date, vendor ID, and booth number from the vendor_booth_assignments
+table.
+*/
+
+Select vendor_id, booth_number, market_date
+from farmers_market.dbo.vendor_booth_assignments
