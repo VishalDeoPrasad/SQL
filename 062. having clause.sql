c53982ecb8daa774f04ffec9cd15506487a3f3db
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-08 12:18:43 +0530

    sql example 62

---
+++ 062. having clause.sql
@@ -0,0 +1,14 @@
+/*
+Filter out vendors who brought atleast 10 quantity items to the farmer's market over the time peroid 2019-04-03 and 2019-05-16.
+*/
+
+
+SELECT 
+    market_date, vendor_id, SUM(quantity) AS qty_for_sale
+FROM
+    vendor_inventory
+WHERE
+    market_date BETWEEN '2019-04-03' AND '2019-05-16'
+GROUP BY vendor_id , market_date
+HAVING SUM(quantity) >= 30
+
