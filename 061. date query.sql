f2e9e1a85dde8a3dbb833b723b717240b066c02c
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-08 12:18:30 +0530

    sql example 61

--- 061. date query.sql
+++ 061. date query.sql
@@ -2,8 +2,12 @@
 Filter out vendors who brought atleast 10 quantity items to the farmer's market over the time peroid 2019-04-03 and 2019-05-16.
 */
 
-select * from vendor_inventory
-where market_date between '2019-04-03' and '2019-05-16' and
-quantity >= 10
-order by market_date, quantity
+
+SELECT 
+    market_date, vendor_id, SUM(quantity) AS qty_for_sale
+FROM
+    vendor_inventory
+WHERE
+    market_date BETWEEN '2019-04-03' AND '2019-05-16'
+GROUP BY vendor_id , market_date
 
