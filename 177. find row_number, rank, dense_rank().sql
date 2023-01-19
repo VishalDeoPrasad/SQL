a74b0429b2eba5b5785198ae2770232c7a2aaa33
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-01-19 19:16:19 +0530

    find row_number, rank and dense rank

---
+++ 177. find row_number, rank, dense_rank().sql
@@ -0,0 +1,22 @@
+/*
+	Problem Statement:
+
+	Write a query to calculate row number and save as 'emp_row_no', rank 
+    and save as 'emp_rank', and the dense rank of employees as 'emp_dense_rank' based on the salary column in descending order 
+    within each department using the employee's table.
+
+	Return the columns 'full_name' (first_name and last_name separated by space), 'department_id', 'salary', 'emp_row_no', 'emp_rank', and 'emp_dense_rank'.
+	Note:
+
+	Select MySQL 8.0 in the drop-down.
+	Use the table employees.
+*/
+
+select concat(first_name, " ", last_name) as full_name,
+		department_id, 
+        salary,
+		row_number() over (partition by department_id order by salary) as emp_row_no,
+        rank() over (partition by department_id order by salary) as emp_rank,
+        dense_rank() over (partition by department_id order by salary) as emp_dense_rank
+        
+from employees
