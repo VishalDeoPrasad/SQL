044e91d55daacb9d5f2dcb3da2c8bed39645dddd
VishalDeoPrasad <vishaldeoprasad@yahoo.com>
2023-04-05 13:19:31 +0530

    combining 2 columns using union

--- 188. combining 2 columns using union function.sql
+++ 188. combining 2 columns using union function.sql
@@ -7,6 +7,8 @@
 	Use the employees table.
 */
 
-select * from employees where department_id = 80 
-		union
-select * from employees where salary > 10000 
+with t as (select * from employees where department_id = 80),
+	x as (select * from employees where salary > 10000)
+select * from t 
+	union 
+select * from x;
